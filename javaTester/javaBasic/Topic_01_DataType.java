845ad1b57c6e8f2416d3aea433fc746802d8f9bd
TuoiNT.Elcom <tuoint@elcom.com.vn>
2026-09-23 17:36:44 +0700

    add Selenium_04_Ex_Commands

---
+++ javaTester/javaBasic/Topic_01_DataType.java
@@ -0,0 +1,20 @@
+package javaBasic;
+
+public class Topic_01_DataType {
+    // Kiểu dữ liệu nguyên thủy (Primitive Type)
+            //Number
+            //Nguyên: Không có thập phân
+            // byte/ short/ int/ long
+
+            // Thực: Có thập phân
+            // float/ double
+
+            // Boolean
+            // boolean
+
+            // character
+            // char
+
+    // Kiểu dữ liệu tham chiếu (Reference Type)
+
+}
